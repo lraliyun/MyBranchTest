d617f5f1d997494492fa0fb7d95c53e99ee59914
huashan_lhc <lr799437029@sina.com>
2019-04-02 20:22:38 +0800

    aaaaaaaaaaaaaaaaaaaa

--- src/com/itcast/test/BranchTest.java
+++ src/com/itcast/test/BranchTest.java
@@ -2,6 +2,6 @@ package com.itcast.test;
 
 public class BranchTest {
     public static void main(String[] args) {
-        System.out.println("老子才是真正的令狐冲，刚才是岳不群冒充我，我在分支feature_lhc上修改");
+        System.out.println("生气了生气了，签名到底怎么改");
     }
 }
