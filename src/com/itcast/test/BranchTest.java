013e7a2f6b1d46b2fc1cad72fb0cfd34f9890eda
lr_glb <lr799437029@aliyun.com>
2019-04-02 17:23:49 +0800

    modify by real lhc

--- src/com/itcast/test/BranchTest.java
+++ src/com/itcast/test/BranchTest.java
@@ -2,6 +2,6 @@ package com.itcast.test;
 
 public class BranchTest {
     public static void main(String[] args) {
-        System.out.println("主分支，不允许在上面操作");
+        System.out.println("老子才是真正的令狐冲，刚才是岳不群冒充我，我在分支feature_lhc上修改");
     }
 }
